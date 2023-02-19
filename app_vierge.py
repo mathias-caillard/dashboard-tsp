6d7e3c59867b0e02e57e9b05273c134815feb692
OverealDev <107404704+OverealDev@users.noreply.github.com>
2023-02-19 16:29:13 +0100

    encodage UTF-8, test d'affichage image locale

--- app_vierge.py
+++ app_vierge.py
@@ -1,4 +1,5 @@
-#Import des bibliothèques
+# coding: utf-8
+#Import des bibliothÃ¨ques
 import dash
 from dash import html
 import os
@@ -6,27 +7,27 @@ import os
 #Initialisation de l'application
 app = dash.Dash(__name__)
 
-#Chemin absolu du répertoire de travail
+#Chemin absolu du rÃ©pertoire de travail
 root_directory = os.path.abspath(os.path.dirname(__file__))
 
 #Chemin relatif de l'image
-image_path = os.path.join(root_directory, 'Images\graphe_tennis.png')
-print(image_path)
+#image_path = os.path.join(root_directory, 'Images\\graphe_tennis.png')
 
-#Définition du layout
+#DÃ©finition du layout
 app.layout = html.Div([
+
     #Titre
     html.H1('Bonjour, voici mon application web vierge en Dash sur Python !'),
 
     #Images
-    #html.Img(src = 'https://rapids.ai/assets/images/Plotly_Dash_logo.png'), (fonctionne avec une source de type lien web)
-    html.Img(src = image_path) #ne fonctionne pas
+    html.Img(src = 'https://rapids.ai/assets/images/Plotly_Dash_logo.png'), #affichage d'une image sur une site web
+    html.Img(src = app.get_asset_url('graphe_tennis.png'))  #affichage d'une image locale
 
 
 ])
 
 
-#Démarrage de l'application
+#DÃ©marrage de l'application
 if __name__ == '__main__':
-    #Activation du débuggage
+    #Activation du debuggage
     app.run_server(debug=True)
