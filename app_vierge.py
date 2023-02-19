2c1c2f8a38c8f598bd863cea73a379dc1bed1d09
OverealDev <107404704+OverealDev@users.noreply.github.com>
2023-02-19 17:18:47 +0100

    plotly test

--- app_vierge.py
+++ app_vierge.py
@@ -1,8 +1,11 @@
 # coding: utf-8
 #Import des bibliothèques
 import dash
-from dash import html
+from dash import html, dcc
 import os
+import pandas as pd
+import plotly.express as px
+
 
 #Initialisation de l'application
 app = dash.Dash(__name__)
@@ -10,8 +13,18 @@ app = dash.Dash(__name__)
 #Chemin absolu du répertoire de travail
 root_directory = os.path.abspath(os.path.dirname(__file__))
 
-#Chemin relatif de l'image
-#image_path = os.path.join(root_directory, 'Images\\graphe_tennis.png')
+#Chemin relatif de l'image graphe_tennis_séparés.png
+image_path = os.path.join(root_directory, 'assets\\graphe_tennis_séparés.png')
+
+
+df = pd.DataFrame({
+    "Année": ["2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019"],
+    "Nb de nationalités étrangères différentes": [34, 39, 41, 40, 42, 43, 45, 43, 45,51]
+})
+
+fig = px.bar(df, x="Année", y="Nb de nationalités étrangères différentes", color = "Nb de nationalités étrangères différentes")
+
+
 
 #Définition du layout
 app.layout = html.Div([
@@ -20,8 +33,15 @@ app.layout = html.Div([
     html.H1('Bonjour, voici mon application web vierge en Dash sur Python !'),
 
     #Images
-    html.Img(src = 'https://rapids.ai/assets/images/Plotly_Dash_logo.png'), #affichage d'une image sur une site web
-    html.Img(src = app.get_asset_url('graphe_tennis.png'))  #affichage d'une image locale
+    #html.Img(src = 'https://rapids.ai/assets/images/Plotly_Dash_logo.png'), #affichage d'une image sur une site web
+    #html.Img(src = app.get_asset_url('graphe_tennis.png')),  #affichage d'une image locale
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+
+    
 
 
 ])
