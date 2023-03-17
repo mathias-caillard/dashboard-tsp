51cd2f2b5d0255abbc4ae239e3d8c7f45bdbb2de
mjacquet <marin.jacquet@telecom-sudparis.eu>
2023-03-17 08:46:05 +0100

    Ajout d'un aspect evolution temporel dans l'onglet DRI

--- app_many_pages/pages/dri.py
+++ app_many_pages/pages/dri.py
@@ -2,7 +2,9 @@ import dash
 from dash import html, dcc
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 from app_many_pages import config
+import random as rd
 
 
 
@@ -58,12 +60,33 @@ fig = px.bar(df, x = "trimestre", y = y_axis)
 fig.update_layout(title = "Chiffres sur l'international à Télécom Sudparis")
 
 
+#Evolution temporelle avec des valeurs simulées
+labels, valeur = [], []
+for i in range(débutColonneTrimestre,débutColonneTrimestre + 4):
+    labels.append(df.columns[i])
+    valeur.append((df.iloc[2,i]))
+
+#Ajout de valeur simulées
+for j in range(5, 17):
+    labels.append("ECOLE T" + str(j))
+    valeur.append(valeur[i%4] + rd.randint(-5, 5))
+
+fig2  = go.Figure(data=[go.Bar(x=labels, y=valeur)])
+
+#Ajout d'un titre
+fig2.update_layout(title = "Evolution dans le temps du nombre d'étudiants étrangers à Télécom Sudparis")
+
+
 layout = html.Div(children=[
     html.H1(children='Bienvenue sur la page concernant la Direction des relations internationales'),
 
     dcc.Graph(
         id='example-graph',
         figure=fig,
+    ),
+    dcc.Graph(
+        id='example-graph',
+        figure=fig2,
     ),
 
     html.Div(children='''
