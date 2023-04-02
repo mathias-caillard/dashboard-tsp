a6b774dee6d9f4477e526b206ea4f56e5d4b0bc1
OverealDev <107404704+OverealDev@users.noreply.github.com>
2023-04-02 21:08:06 +0200

    UI page principale

--- app_many_pages/app.py
+++ app_many_pages/app.py
@@ -1,26 +1,56 @@
 from dash import Dash, html, dcc
 import dash
+import dash_bootstrap_components as dbc
+from dash_bootstrap_templates import load_figure_template
 
 
-app = Dash(__name__, use_pages=True)
+
+
+app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
+load_figure_template("bootstrap")
 app._favicon = "favicon.ico"
 
-app.layout = html.Div([
-	html.H1('Application multipage pour la visualisation d\'indicateurs'),
-
+app.layout = dbc.Container([
+	
+        dbc.NavbarSimple(
+            children=[
+                """dbc.NavItem(dbc.NavLink("Page 1", href="#")),
+                dbc.DropdownMenu(
+                    children=[
+                        dbc.DropdownMenuItem("Plus de pages", header=True),
+                        dbc.DropdownMenuItem("Page 2", href="#"),
+                        dbc.DropdownMenuItem("Page 3", href="#"),
+                    ],
+                    nav=True,
+                    in_navbar=True,
+                    label="...",
+                ),"""
+            ], 
+            brand="Indicateurs de Télécom SudParis",
+            brand_href="#",
+            color="dark",
+            dark=True,
+	    ),
+	    
     html.Div(
         [
-            html.Div(
-                dcc.Link(
-                    f"{page['name']}", href=page["relative_path"]
+	
+    
+ 
+                dbc.Button(
+                    f"{page['name']}", href=page["relative_path"], outline=True, color="dark", className="me-1"
                 )
-            )
+
             for page in dash.page_registry.values()
+	    
         ]
     ),
+	dash.page_container,
+	
 
-	dash.page_container
-])
+],
+fluid = True,
+)
 
 if __name__ == '__main__':
 	app.run_server(debug=True)
