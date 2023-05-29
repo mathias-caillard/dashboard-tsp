f2e6f392a53a2d3bdc4acc5d8bbd3769d581e9c6
mjacquet <marin.jacquet@telecom-sudparis.eu>
2023-05-29 08:47:08 +0200

    maj titres (avec code indicateur)

--- src/functions/fonctions_historique.py
+++ src/functions/fonctions_historique.py
@@ -5,7 +5,7 @@ from dash import html, dcc, Output, Input, State, callback
 import dash_bootstrap_components as dbc
 from src.fig import daf_fig, df_fig, dire_fig, drfd_fig, drh_fig, dri_fig, artemis_fig, citi_fig, eph_fig, inf_fig, rs2m_fig, rst_fig
 from src import config
-import data
+
 import plotly.express as px
 import plotly.graph_objects as go
 from sklearn.preprocessing import LabelEncoder
